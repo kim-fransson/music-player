6f4d116595fe42d1464ec6ad3c0c30c6dfcddbe4
Kim Fransson <kim.fransson@sigmatechnology.se>
2022-10-18 12:25:42 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1> Hello, World </h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.([cm]?ts|tsx)$/,
+        loader: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    extensionAlias: {
+      ".js": [".js", ".ts"],
+      ".cjs": [".cjs", ".cts"],
+      ".mjs": [".mjs", ".mts"],
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    static: path.join(__dirname, "public/"),
+    devMiddleware: {
+      publicPath: "/dist/",
+    },
+    port: 3000,
+    hot: "only",
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+};
